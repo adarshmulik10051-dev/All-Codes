58f0e3d1c50e9802cd6dd211c7820d1200769a65
adarsh mulik <adarshmulik10051@gmail.com>
2026-02-05 23:30:02 +0530

    Added Merge Sort using Divide and Conquer

---
+++ DSA/Divide_and_Conquer/Merge_sort.java
@@ -0,0 +1,63 @@
+package DSA.Divide_and_Conquer;
+
+public class Merge_sort {//it is depth first ms hai
+    public static void print_arr(int arr[]){
+        //for printing array
+        for(int i = 0 ; i < arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void merge_sort(int arr[],int si , int ei){
+        if(si>ei){
+            return;
+        }
+        if(si==ei){
+            return;
+        }
+        int mid = si+(ei-si)/2;
+
+        merge_sort(arr,si,mid);// left part
+        merge_sort(arr,mid+1,ei);//right part
+         merge(arr,si,ei,mid);//merging
+    }
+    public static void merge(int arr[], int si, int ei, int mid){
+        //if si=0;ei=5 then array size is ei-si+1
+        int temp[]= new int [ei-si+1];
+        int i =si;//idx for first sorted part
+        int j =mid+1;//idx for 2nd sorted part
+        int k = 0 ;//idx for temp arr
+
+        while(i <= mid && j <=ei ){
+            if(arr[i]<arr[j]){
+                temp[k]= arr[i];
+                i++;
+                k++;
+            }
+            else{
+                temp[k]=arr[j];
+                j++;
+                k++;
+            }
+            // left over element for 1st sorted part
+            while(i<=mid){
+                temp[k++]=arr[i++];
+            }
+
+            //right over element for 1st sorted part
+            while(j<=ei){
+                temp[k++]=arr[j++];
+            }
+        }
+        //copy for temp array to arr
+        for( k = 0 ,i = si ; k<temp.length;k++,i++){
+            arr[i]=temp[k];
+        }
+
+    }
+    public static void main (String args[]){
+        int arr[]={5,8,3,7,4,5,3,1,9};
+        merge_sort(arr,0,arr.length-1);
+        print_arr(arr);
+
+    }
+}
