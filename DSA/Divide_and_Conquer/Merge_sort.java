77bff620fa1ca5456752ce74317c82ba00d8bcea
adarsh mulik <adarshmulik10051@gmail.com>
2026-02-09 20:08:54 +0530

    modified  Merge_sort file

--- DSA/Divide_and_Conquer/Merge_sort.java
+++ DSA/Divide_and_Conquer/Merge_sort.java
@@ -38,15 +38,16 @@ public class Merge_sort {//it is depth first ms hai
                 j++;
                 k++;
             }
-            // left over element for 1st sorted part
-            while(i<=mid){
-                temp[k++]=arr[i++];
-            }
 
-            //right over element for 1st sorted part
-            while(j<=ei){
-                temp[k++]=arr[j++];
-            }
+        }
+        // left over element for 1st sorted part
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+
+        //right over element for 1st sorted part
+        while(j<=ei){
+            temp[k++]=arr[j++];
         }
         //copy for temp array to arr
         for( k = 0 ,i = si ; k<temp.length;k++,i++){
@@ -55,7 +56,7 @@ public class Merge_sort {//it is depth first ms hai
 
     }
     public static void main (String args[]){
-        int arr[]={5,8,3,7,4,5,3,1,9};
+       int arr[]={1,2,4,3,5,6};
         merge_sort(arr,0,arr.length-1);
         print_arr(arr);
 
