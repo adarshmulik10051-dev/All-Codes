7c7dc4c4e80e6116d1bd79c0efea8bdabbc855de
adarsh mulik <adarshmulik10051@gmail.com>
2026-04-16 19:11:58 +0530

    learn condition statements

---
+++ WebDev/JavaScript/Level2/3conditional_Statement.js
@@ -0,0 +1,72 @@
+//1)IF statement--->
+
+let age = 18 ; 
+console.log("before if work");
+if (age>=18){
+    console.log("you can drive ");
+}
+console.log("after if work ");
+
+//2) p q signal traffic -->
+
+let color="red";
+
+ if(color=="red"){
+    console.log("now stop!!");
+ }
+ if(color=="yellow"){
+    console.log("get slow now");
+ }
+ if(color=="green"){
+    console.log("go ahead");
+ }
+
+ //3) else if statement 
+
+ let marks = 75;
+
+ if(marks>=75){
+    console.log("you pass!!");
+ }
+ else if(marks<=75){
+    console.log("fail!!");
+ }
+
+ //4) else codition 
+
+ let balance= 800;
+  if(balance>=500){
+    console.log(` you can pay &account balance is ${balance} `);
+  }else{
+    console.log("insufficient bank balance!!")
+  }
+
+  //5) Practice question
+  
+  let size="s";
+
+  if(size== "xl"){
+    console.log("price is RS.250");
+  }
+else if(size=="l"){
+    console.log("price is RS.200 ");
+}else if(size=="m"){
+    console.log("price is RS.150");
+}
+ if(size=="s"){
+    console.log("price is RS. 100");
+}
+
+//6)if - else
+
+     let marks = 35;
+     if(marks>=35){
+        console.log("pass");
+        if(marks>=80){
+            console.log("grade is : o+");
+        }else{
+            console.log("grade is A");
+        }
+     }else{
+        console.log("Better luck next time");
+     }
