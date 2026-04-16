69dad1c9be82a2dd2559e3aee46b457bf31db169
adarsh mulik <adarshmulik10051@gmail.com>
2026-04-16 17:57:02 +0530

    learn console how to link operators

---
+++ WebDev/JavaScript/Level2/app.js
@@ -0,0 +1,12 @@
+    console.log("Hello world!!");
+    console.log("Adarsh MUlik!");
+    let a = 10 ;
+    let b = 5;
+    console.log("Sum is :",a+b);
+
+    //template literals
+    
+    let pencil=10;
+    let pen = 5;
+    let output=`The total price is ${pencil+pen} ruppes`;
+    console.log(output);
