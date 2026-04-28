377059a7f52712304ea38c51a5998adfa9bfa1cc
adarsh mulik <adarshmulik10051@gmail.com>
2026-04-28 21:42:08 +0530

    learn manuplate the attributes

--- WebDev/JavaScript/Level9_DOM/app.js
+++ WebDev/JavaScript/Level9_DOM/app.js
@@ -35,3 +35,40 @@ console.dir(qs);
 
 let all=document.querySelectorAll('div a');
 console.dir(all);
+
+// text &content base property
+//1. innerText-->it just what display on page exact give
+
+document.querySelector('p').innerText;
+//you can easilt change vaule but it not recognize the markup launguage trat as text 
+
+
+//2. innerHTML
+// it will you full markup html code with all tags that not visible on web page text b u i etc.
+    document.querySelector('p').innerHTML;
+
+//3. textContent-->It will give vs code  line paces and next line 
+document.querySelector('p').textContent;
+
+// getAttribute('att');
+
+let img = document.querySelector('img');
+
+img.getAttribute('id');
+
+//setAttribute('att' , 'value') ;  value= that yyou want to change add there 
+
+let para = document.querySelector('p');
+para.setAttribute('p','abcd');
+
+
+// manuplate style using style property
+
+let image = document.querySelector('.oldImg');
+console.dir(image);
+console.dir( img.style);
+
+let links = DocumentFragment.querySelector('.box a');
+ for(link of links ){
+    link.style.color="purple";
+ }
