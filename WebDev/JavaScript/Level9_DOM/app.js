183d797542f11bc0fc6df8b72b1d3b7546175d0b
adarsh mulik <adarshmulik10051@gmail.com>
2026-05-05 19:12:36 +0530

    learn manuplating style acess,add,remove, element

--- WebDev/JavaScript/Level9_DOM/app.js
+++ WebDev/JavaScript/Level9_DOM/app.js
@@ -60,8 +60,7 @@ let img = document.querySelector('img');
 
 img.getAttribute('id');
 
-let a = document.querySelector('a');
-a.getAttribute('class');
+
 
 //setAttribute('att' , 'value') ;  value= that yyou want to change add there 
 
@@ -69,15 +68,109 @@ let para = document.querySelector('p');
 para.setAttribute('p','abcd');
 
 
-// manuplate style using style property
+// manuplate styl
+// 1) using style property:
 
+//element.style to see css property
 let image = document.querySelector('.oldImg');
 console.dir(image);
 console.dir( img.style);
 
-let links = Document.querySelector('.box a ');
+// acess and aplly style on the element,here we use camelCase not css style like hypens like 
+//element.style.color=''; 
 
- for(link of links ){
-    link.style.color="purple";
- }
+let heading = document.querySelector('h1');
+heading.style.color='orange';// this style will run  on inly console not in file 
+
+
+let links = document.querySelectorAll('.box a');
+
+for(link of links){
+    link.style.color='red';
+}
+
+//2) using classList property
+ // it helps to add classe 
+
+ let smallphoto =document.querySelector('.images');
+
+console.dir( smallphoto.classList);
+
+//functions 
+
+
+//1) .classList.add(""); // to add class name in list 
+    smallphoto.classList.add("imgg");
+
+//2) .classList.remove(""); // to remove class name in list 
+    smallphoto.classList.remove("imgg");
+
+//3) .classList.contain(""); // ya adhi present ahhe ka   class name  
+        smallphoto.classList.contains("imgg");
+
+//4) .classList.toggle(""); // to add or remove class name  like pahila asel tr remove nasel tr add 
+    smallphoto.classList.toggle("imgg");
+
+// set style in  css class boxbg name now we simple add class then it is easilt aplly on this elemnt
+
+let b = document.querySelector('.box');
+b.classList.add("boxbg");
+
+
+// navigation on page 
+
+let di= document.querySelector('div');
+
+//1).parentElemnt --> parent ellemt deto 
+    di.parentElement;//print karava lagel 
+
+//2).children-->to give child of acessed element
+    di.children;
+
+//3).chileElementCount--> return count of chile element
+    di.childElementCount;
+
+//4)previousElementSibling
+  console.dir(  di.previousElementSibling);
+//5)nextElementSibling   
+console.dir(di.nextElementSibling); 
+ // you can easily change syyle also
+
+ // adding Elements 
+//first create 
+ let newp = document.createElement('p');
+ newp.innerText="hii i am adarsh ";
+ let box = document.querySelector('.box');
+
+//element(inside).appendChild(elemnt who pass );
+ box.appendChild(newp);
+
+ //also we can create buttun 
+ let btn = document.createElement('button');
+ btn.innerText="click me";
+ box.appendChild(btn);
+
+
+ // only use append()--> tych element add honar 
+
+ newp.append(" student of college of");//this text add into newp // btn sudha add kru shakto 
+
+ //apend add to last 
+ //prepend(element);
+box.prepend(btn);
+
+//insertAdajecentElement(where,element);
+     let p = document.querySelector('p');
+     let newbtn = document.createElement('button');
+     newbtn.innerText="Submit";
+//where--> beforebegin 
+    p.insertAdjacentElement('beforeBegin',newbtn);
+    p.insertAdjacentElement('afterBegin',newbtn);
+    p.insertAdjacentElement('beforeend',newbtn);
+    p.insertAdjacentElement('afterend',newbtn);
+
+
+
+
+
 
