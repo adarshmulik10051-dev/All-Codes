b7d2578b804e07c8c092da2b1dd70327c173f3a5
adarsh mulik <adarshmulik10051@gmail.com>
2026-06-20 14:10:05 +0530

    solve arrays basic problem

--- DSA_rev/Basic_arrays.java
+++ DSA_rev/Basic_arrays.java
@@ -145,22 +145,132 @@ public class Basic_arrays{
                 }
             }
             System.out.println();
+
+            //8.questions by apna college 
+            //8.1Largest in array o(n)
+            int uni[]={1,2,3,4,5};
+            int n1 = uni.length;
+            int maximum = Integer.MIN_VALUE;
+            for(int i = 0 ; i < uni.length;i++){
+                if(maximum<uni[i]){
+                    maximum=uni[i];
+                }
+            }
+            System.out.println("maximum number in array is : "+maximum);
+
             
+            //8.2reverse an array--> o(N)
+            int start1 = 0;
+            int end1=uni.length-1;
+            while(start1 < end1){
+                int temp=uni[start1];
+                    uni[start1]=uni[end1];
+                    uni[end1]=temp;
+                    start1++;
+                      end1--;
+            }
+            
+            for(int revarr : uni){
+                System.out.print(revarr+", ");
+            }
+            System.out.println();
 
+            //8.3 pairs in array o(n^2) 
+            for(int i = 0 ; i <n1 ;i++){
+                for(int j = 0 ; j <n1 ; j++){
+                    System.out.print("("+uni[i]+","+uni[j]+")");
+                }
+                System.out.println();
+            }
+            System.out.println();
 
+            //8.4 print subarrays o(n^3)
+            for(int i = 0 ; i < n1 ; i ++){
+                for(int j = i  ; j < n1 ; j++){
+                    for(int k = i ; k <=j ; k++){
+                        System.out.print(uni[k]+",");
+                    }
+                    System.out.println();
+                }
+            }
+            System.out.println();
+
+            //8.5 maximumsum subarray
+            //8.5.1 brute force--o(n^3)
+            int maxsum= Integer.MIN_VALUE;
+            for (int i = 0 ; i < n1 ; i ++){
+                for(int j = i ; j<n1 ; j++){
+                    int sum = 0 ; 
+                    for(int k = i ; k<=j ; k++){
+                        sum+=uni[k];
+                    }
+                    if(maxsum<sum){
+                        maxsum =sum;
+                    }
+                }
+            }
+            System.out.println("maximum sum of subarray is :"+maxsum);
+
+            //8.5.2 prefixsum:o(n)+o(n^2)
+            int currsum=0;
+            int prefix []= new int [n1];
+            prefix[0]=uni[0];
+            for(int i = 1; i <prefix.length; i++){
+                prefix[i]=uni[i]+prefix[i-1];
+            } 
+            int maxs =Integer.MIN_VALUE;
+            for(int i = 0 ; i < n1 ;i++){
+                for(int j = i ;j<n1 ; j++ ){
+                    currsum=i==0?prefix[j]:prefix[j]-prefix[i-1];
+                    if(maxs<currsum){
+                        maxs=currsum;
+                    }
+                }
+               
+            }
+            System.out.println("maxsumsubarray is :"+maxs);
              
+            //8.5.3 Optimal solution (kadan's algo):o(n)
+            int cs = 0 ;
+            int ms = Integer.MIN_VALUE;
+            for(int i = 0 ; i < n1 ; i ++){
+                cs +=uni[i];
+                if(cs<0){
+                    cs=0;
+                }
+                ms=Math.max(cs,ms);
+            }
+            System.out.println("Maxsum is :"+ms);
+            
+            //8.6 Trapped rain water problem 
+          
 
+            int [] height={4,2,0,6,3,2,5};
+            int length=height.length;
             
-                
+            //calculate leftmax boundry
+            int leftMax[]=new int[length];
+            leftMax[0]=height[0];
+            for(int i = 1; i < length ; i ++){
+                leftMax[i]=Math.max(leftMax[i-1],height[i]);
+            }
 
-
+            //calculate rightMax boundry
+            int rightMax[]=new int [length];
+            rightMax[length-1]=height[length-1];
+            for(int i =length-2 ; i >=0 ; i--){
+                rightMax[i]=Math.max(rightMax[i+1],height[i]);
+            }
+             int trapedWater=0;
+            for(int i = 0 ; i < length ; i ++){
+                //calculate waterlevel
+                int waterlevel=Math.min(leftMax[i], rightMax[i]);
+            //calculate traped water 
+             trapedWater += waterlevel-height[i];
             
-
-
-            
-            
-            
-            
+            }
+            System.out.println("tp is ; "+trapedWater);
+           
     
     }
 }
